a55face8b8494f6098ffa7b6683f7a2ffd6be1e7
Sharpryan20 <sharpryan01@icloud.com>
2022-03-22 18:00:04 +0000

    connected index.html url in the urls.py file

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Recipe 
+from .models import Recipe
 
 
-class RecipeList(generic.Listview):
+class RecipeList(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
-    template_name - 'index.html'
+    template_name = 'index.html'
     paginate_by = 10

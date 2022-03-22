0bf2c52917cbe1c69685a4fb00b02b3fe30278b5
Sharpryan20 <sharpryan01@icloud.com>
2022-03-22 17:02:46 +0000

    created class RecipeAdmin in admin.py and added filter searchers to admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Recipe 
+from .models import Recipe, Comment 
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'author', 'status', 'created_on')
+    search_fields = ('title', 'ingredients', 'serving_size')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('ingredients', 'instructions')
 

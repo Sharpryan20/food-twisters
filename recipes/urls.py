f5088dbeb12180ad323a36be1905accdfc6ad758
Sharpryan20 <sharpryan01@icloud.com>
2022-03-22 20:13:04 +0000

    created view code for recipe_detail and hooked up the URL

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.RecipeList.as_view(), name='home')
+    path('', views.RecipeList.as_view(), name='home'),
+    path('recipe/', views.RecipeList.as_view(), name='recipe'),
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,32 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
 class RecipeList(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
+    template_name = 'recipe.html'
     paginate_by = 10
 
+
+class RecipeDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "liked": liked
+            }
+        )
 

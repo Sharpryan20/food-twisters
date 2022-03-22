db5f60e798f7173225f0935a86946d176c3ef484
Sharpryan20 <sharpryan01@icloud.com>
2022-03-22 17:29:33 +0000

    created view code for RecipeList

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Recipe 
 
-# Create your views here.
+
+class RecipeList(generic.Listview):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+    template_name - 'index.html'
+    paginate_by = 10

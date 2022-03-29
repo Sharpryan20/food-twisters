6153f74b5bacfb4793787c8857c1d477fe23432b
Sharpryan20 <sharpryan01@icloud.com>
2022-03-29 17:55:17 +0000

    fixed the problem with urls not working by removing a path in food twisters urls.py

--- food_twisters/urls.py
+++ food_twisters/urls.py
@@ -20,5 +20,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('recipes.urls')),
-    path('recipes/', include('recipes.urls')),
 ]
--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,16 @@ from . import views
 from django.urls import path
 
 #base_url/recipes
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('recipe/', views.RecipeList.as_view(), name='recipe'),
+#     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
+#     path('category/', views.CategoryList.as_view(), name='category'),
+# ]
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('recipe/', views.RecipeList.as_view(), name='recipe'),
-    path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
+    path('post/', views.RecipeList.as_view(), name='post'),
     path('category/', views.CategoryList.as_view(), name='category'),
+    path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
 ]

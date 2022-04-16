55cd70516fdf3a066ad4d7a49fc3fe93e0d1fcb7
Sharpryan20 <sharpryan01@icloud.com>
2022-04-16 15:34:26 +0000

    installed and set up Django allauth

--- food_twisters/urls.py
+++ food_twisters/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('recipes.urls')),
+    path('accounts/', include('allauth.urls'))
 ]

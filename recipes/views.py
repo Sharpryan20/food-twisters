b0c0e115a6d44f7979d9124478b9163c83cb97e9
Sharpryan20 <sharpryan01@icloud.com>
2022-04-23 21:00:51 +0000

    fixed some spelling mistakes within 'def get' in class RecipeDetail

--- recipes/views.py
+++ recipes/views.py
@@ -34,24 +34,24 @@ class RecipeDetail(View):
         )
     
     def post(self, request, slug, *args, **kwargs):
+
         queryset = Recipe.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
-        comments = recipe.comments.filter(approved=True).order_by('created_on')
+        comments = recipe.comments.filter(approved=True).order_by("-created_on")
         liked = False
         if recipe.likes.filter(id=self.request.user.id).exists():
             liked = True
 
         comment_form = CommentForm(data=request.POST)
-
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
             comment_form.instance.name = request.user.username
             comment = comment_form.save(commit=False)
-            comment.post = post
+            comment.post = recipe
             comment.save()
         else:
             comment_form = CommentForm()
-        
+
         return render(
             request,
             "recipe_detail.html",
@@ -59,8 +59,8 @@ class RecipeDetail(View):
                 "recipe": recipe,
                 "comments": comments,
                 "commented": True,
-                "liked": liked,
-                "comment_form": CommentForm()
+                "comment_form": comment_form,
+                "liked": liked
             },
         )
 
